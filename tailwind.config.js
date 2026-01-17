519fbccc346ec8aa5cfbc8ca9c9db32d16e711e5
catherinereen <cngotran@uwaterloo.ca>
2026-01-17 12:29:32 -0500

    added interests page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: ["./index.html", "./interests/index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {}
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
-export default defineConfig({
-  base: "/personal-website/"
-});
+export default defineConfig(({ mode }) => ({
+  base: "/personal-site/",
+}));
