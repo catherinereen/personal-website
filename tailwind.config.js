e4bde686d99cb667c58e3fe9031a91ced57412a2
catherinereen <cngotran@uwaterloo.ca>
2026-01-17 12:38:56 -0500

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./interests/index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: [
+    "./index.html",
+    "./interests/index.html",
+    "./wallstreetbets/index.html",
+    "./src/**/*.{ts,tsx,js,jsx}"
+  ],
   theme: {
     extend: {}
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  base: "/personal-website/",
+  base: mode === "development" ? "/" : "/personal-website/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        interests: resolve(__dirname, "interests/index.html"),
+        wallstreetbets: resolve(__dirname, "wallstreetbets/index.html")
+      }
+    }
+  }
 }));
